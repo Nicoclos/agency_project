9283156bc7cfa63af78c5c138db0749e2c06ef3e
jordan-creyelman <creyelmanjo@gmail.com>
2022-02-08 17:35:59 +0100

    dark mode

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "./themes.js";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const StyledApp = styled.div`
+  color: ${(props) => props.theme.fontColor};
+`;
+
+function App() {
+  const [theme, setTheme] = useState("light");
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
+  return (
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <StyledApp>
+        Hello World
+        <button onClick={() => themeToggler()}>Change Theme</button>
+      </StyledApp>
+    </ThemeProvider>
+  );
 }
 
 export default App;
