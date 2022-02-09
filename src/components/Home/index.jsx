dc057cb34d331615e934d99639cbe4622c5a5dcf
jordan-creyelman <creyelmanjo@gmail.com>
2022-02-09 12:53:14 +0100

    texte home

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Home = () =>{
   return (
     <div>
-        
+       <p>Grâce à notre savoir-faire, notre expérience et notre écoute, nous accompagnons nos clients dans la création de site internet: étude, UX, conception, design, développement, SEO.  </p>
+       <p>Notre agence web est capable de répondre à tous vos besoins et d'élaborer une véritable stratégie digitale. </p>
     </div>
   );
 }
